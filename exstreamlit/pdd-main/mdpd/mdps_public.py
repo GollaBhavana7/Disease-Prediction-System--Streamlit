6f31cb1257998f3cf2268191a23efa3d8a33d4fb
GollaBhavana7 <161456145+GollaBhavana7@users.noreply.github.com>
2024-12-01 23:23:47 +0530

    Update mdps_public.py

--- exstreamlit/pdd-main/mdpd/mdps_public.py
+++ exstreamlit/pdd-main/mdpd/mdps_public.py
@@ -203,10 +203,10 @@ if st.session_state.logged_in:
 
                 # Tabular Data
                 test_data = {
-                    st.markdown(f"**Parameter Name": [
+                    st.markdown(f"**Parameter Name"): [
                         "Pregnancies", "Glucose", "Blood Pressure", "Skin Thickness", 
                         "Insulin", "BMI", "Diabetes Pedigree Function"
-                    ]),
+                    ],
                     "Patient Values": [
                         Pregnancies, Glucose, BloodPressure, SkinThickness, 
                         Insulin, BMI, DiabetesPedigreeFunction
